df51596f441ee59d5aa359d30d636a7a7f8f00f8
Julien <juli1.charvet@gmail.com>
2024-12-15 01:47:48 +0100

    Added navmesh surface and obstacle

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -5,12 +5,14 @@ public class Chest : MonoBehaviour, IInteractable
 	public Animator animator;
 	private PopUpController popUpController;
 	private bool opened = false;
+	private AudioSource sfx;
 
 	public ItemData item;
 
 	private void Start()
 	{
 		popUpController = FindAnyObjectByType<PopUpController>(FindObjectsInactive.Include);
+		sfx = GetComponent<AudioSource>();
 	}
 
 	public void Interact()
@@ -21,5 +23,6 @@ public class Chest : MonoBehaviour, IInteractable
 		popUpController?.SetupPopup(item);
 		popUpController?.ShowPopUp();
 		animator.SetBool("isOpen", true);
+		sfx.Play();
 	}
 }
